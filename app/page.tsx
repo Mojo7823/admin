1f23e7647d91a6a0b63b70f5d03f2dba84902614
bagus <bagusatmaja80@icloud.com>
2024-03-17 16:47:37 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import CraftContent from "./components/CraftContent";
-import Details from "./components/Details";
-import Download from "./components/Download";
-import Footer from "./components/Footer";
+//import Details from "./components/Details";
+//import Download from "./components/Download";
+//import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Icons from "./components/Icons";
 import Social from "./components/Social";
 import Statement from "./components/Statement";
-import Testimonials from "./components/Testimonials";
+//import Testimonials from "./components/Testimonials";
 
 const Home = () => {
 	return (
@@ -16,14 +16,17 @@ const Home = () => {
 				<Hero />
 			</Header>
 			<Icons />
-			<Details />
 			<Statement />
 			<CraftContent />
-			<Testimonials />
-			<Download />
+
 			<Social />
-			<Footer />
+
 		</>
 	);
 };
 export default Home;
+
+//			<Footer />
+//<Testimonials />
+//<Download />
+//			<Details />
