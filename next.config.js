99002d2c5ffd83a594fd28c3eb963fcd0debdab3
bagus <bagusatmaja80@icloud.com>
2024-03-17 14:00:27 +0700

    new new

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  trailingSlash: true, // or false, depending on your preference
+}
+ 
+module.exports = nextConfig
