b6fb28ac7e68071c2693324c1f498b80f076b9ac
bagus <bagusatmaja80@icloud.com>
2024-03-19 14:00:32 +0700

    update dikit

--- pages/api/customer.js
+++ pages/api/customer.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     const [rows] = await db.execute("SELECT id,kode_pel,nama, alamat, alamat2, no_hp, keterangan, status FROM customer");
     res.json(rows);
   } catch (error) {
-    console.error('An error occurred:', error);
-    res.status(500).json({ message: 'An error occurred.' });
+    console.error('General Database error:', error);
+    res.status(500).json({ message: 'Database Eror, Coba cek customer.js.' });
   }
 }
